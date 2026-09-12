f65b9aa4877cec5888882db373a748be5e6e3fe3
unknown <vsgabcd48@gmail.com>
2026-09-12 12:55:48 +0700

    update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/library-flow-favicon.svg", type: "image/svg+xml" },
     ],
   }),
   shellComponent: RootShell,
